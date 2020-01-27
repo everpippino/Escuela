cdb42869b2bd55393d7d3e76606c617d71293f5c
everpippino <everpippino@gmail.com>
2020-01-27 19:50:10 -0300

    migracion y modelo de division

---
+++ app/Division.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Division extends Model
+{
+  protected $table = "division";
+
+  protected $fillable = [
+    'aula',
+    'nombre',
+  ];
+
+  public function anio(){
+    //Relacion de muchas divisiones a un año 
+          return $this->belongsTo('App\Anio');
+  }
+  public function matricula(){
+    //Relacion de una division a muchas matriculas 
+          return $this->hasMany('App\Matricula');
+  }
+}
