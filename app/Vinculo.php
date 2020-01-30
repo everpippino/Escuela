2b47c963238dcca0bb0fa4457bad62dd4927a117
everpippino <everpippino@gmail.com>
2020-01-30 19:20:00 -0300

    migracion y modelo de vinculo

---
+++ app/Vinculo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vinculo extends Model
+{
+  protected $table = "vinculo";
+
+  protected $fillable = [
+    'nombre'
+  ];
+
+ }
